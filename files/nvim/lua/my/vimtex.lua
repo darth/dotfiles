56f1f9969e83e1190f3c4ed39fa23b6a838cfc89
Vadim Alimguzhin <alimguzhin@di.uniroma1.it>
2021-02-20 18:14:35 +0100

    [VIM] Stop using pplatex for vimtex quickfix parsing.

--- files/nvim/lua/my/vimtex.lua
+++ files/nvim/lua/my/vimtex.lua
@@ -8,7 +8,7 @@ g.latex_fold_enabled = true
 
 if (fn.has('mac')) then g.vimtex_view_method = 'skim' end
 
-if (fn.executable('pplatex')) then g.vimtex_quickfix_method = 'pplatex' end
+-- if (fn.executable('pplatex')) then g.vimtex_quickfix_method = 'pplatex' end
 
 g.vimtex_compiler_latexmk = {build_dir = 'build'}
 g.vimtex_compiler_progname = 'nvr'
