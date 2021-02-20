d94e4113defbb9e10321c02e8a7880b45da1de6b
Vadim Alimguzhin <alimguzhin@di.uniroma1.it>
2021-02-20 17:11:15 +0100

    [VIM] Fix git commit mapping.

--- files/nvim/lua/my/fugitive.lua
+++ files/nvim/lua/my/fugitive.lua
@@ -1,5 +1,5 @@
 local map = require'my.utils'.map
 
 map('n', '<leader>gs', '<cmd>Gstatus<CR>', {silent = true})
-map('n', '<leader>gc', '<cmd>Gcommit<CR>', {silent = true})
+map('n', '<leader>gc', '<cmd>Git commit<CR>', {silent = true})
 map('n', '<leader>gd', '<cmd>Gdiff<CR>', {silent = true})
