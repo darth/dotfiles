2a875fecf9edd94686a3e17b37019939fb33f7af
Vadim Alimguzhin <alimguzhin@di.uniroma1.it>
2021-02-22 16:34:45 +0100

    [VIM] Use rounded separators in lualine.

--- files/nvim/lua/my/statusline.lua
+++ files/nvim/lua/my/statusline.lua
@@ -1,5 +1,7 @@
 local lualine = require 'lualine'
 
 lualine.options.theme = 'nord'
+lualine.options.section_separators = {'', ''}
+lualine.options.component_separators = {'', ''}
 lualine.extensions = {'fzf'}
 lualine.status()
