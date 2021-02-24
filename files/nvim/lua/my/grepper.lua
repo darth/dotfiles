b28cc678f2fdee944e5699c6a27e6c8f8f9ef9a2
Vadim Alimguzhin <alimguzhin@di.uniroma1.it>
2021-02-24 22:08:03 +0100

    [VIM] Change GrepperGrep to GrepperRg for grep command alias.

--- files/nvim/lua/my/grepper.lua
+++ files/nvim/lua/my/grepper.lua
@@ -8,4 +8,4 @@ map('n', '<leader>*', '<cmd>Grepper -cword -noprompt<CR>', {})
 
 map('nx', 'gs', '<plug>(GrepperOperator)', {noremap = false})
 
-command_alias('grep', 'GrepperGrep')
+command_alias('grep', 'GrepperRg')
