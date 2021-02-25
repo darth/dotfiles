fa280964b34a014311bc1e63f874f39f89bb8dc2
Vadim Alimguzhin <alimguzhin@di.uniroma1.it>
2021-02-25 19:14:43 +0100

    [VIM] Add diagnostics component to statusline.

--- files/nvim/lua/my/statusline.lua
+++ files/nvim/lua/my/statusline.lua
@@ -1,6 +1,12 @@
 local lualine = require 'lualine'
 
 lualine.options.theme = 'nord'
+lualine.sections.lualine_x = {
+  'encoding', 'fileformat', 'filetype', {'diagnostics', sources = {'nvim_lsp'}},
+}
+lualine.sections.lualine_z = {
+  {'location', icon = ''}
+}
 lualine.options.section_separators = {'', ''}
 lualine.options.component_separators = {'', ''}
 lualine.extensions = {'fzf'}
