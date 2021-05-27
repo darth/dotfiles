936ba82b62b3977244ab889e12e97834a68972ac
Vadim Alimguzhin <alimguzhin@di.uniroma1.it>
2021-05-27 18:39:14 +0200

    [VIM] Make sourcegraph_js LSP server configuration compatible with actual version of nvim-lspconfig.

--- files/nvim/lua/my/lsp.lua
+++ files/nvim/lua/my/lsp.lua
@@ -8,6 +8,8 @@ api.nvim_exec([[
   sign define LspDiagnosticsSignHint text=➤ texthl=LspDiagnosticsHint
 ]], false)
 
+require('lspconfig/sourcegraph_js')
+
 local nvim_lsp = require('lspconfig')
 local on_attach = function(client, bufnr)
   -- Mappings.
