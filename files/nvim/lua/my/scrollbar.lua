9d99c325f9e5c3557fddedb019e82501d657b429
Vadim Alimguzhin <vadim.alimguzhin@gmail.com>
2021-06-30 13:09:50 +0200

    [VIM] Update scrollbar configuration.

--- files/nvim/lua/my/scrollbar.lua
+++ files/nvim/lua/my/scrollbar.lua
@@ -3,5 +3,5 @@ local augroup = require'my.utils'.augroup
 augroup('ScrollbarInit', {
   {'CursorMoved,VimResized,QuitPre', '*', 'silent! lua require("scrollbar").show()'},
   {'WinEnter,FocusGained', '*', 'silent! lua require("scrollbar").show()'},
-  {'WinLeave,FocusLost', '*', 'silent! lua require("scrollbar").clear()'}
+  {'WinLeave,BufLeave,BufWinLeave,FocusLost', '*', 'silent! lua require("scrollbar").clear()'}
 })
